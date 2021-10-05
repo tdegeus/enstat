6243df4930a93b8812154db0d173dd4bc86c5217
Tom de Geus <tom@geus.me>
2021-10-05 16:32:59 +0200

    Adding test with defaultdict

--- tests/main.py
+++ tests/main.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import defaultdict
 
 import numpy as np
 
@@ -75,6 +76,42 @@ class Test_mean(unittest.TestCase):
         self.assertTrue(np.allclose(average.std(), np.array([0, 0, 0])))
         self.assertTrue(average.shape() == [3])
         self.assertTrue(average.size() == 3)
+
+
+class Test_defaultdict(unittest.TestCase):
+    def test_Scalar(self):
+
+        average = defaultdict(enstat.mean.Scalar)
+
+        a = np.random.random(50 * 20).reshape(50, 20)
+        b = np.random.random(52 * 21).reshape(52, 21)
+
+        for i in range(a.shape[0]):
+            average["a"].add_sample(a[i, :])
+
+        for i in range(b.shape[0]):
+            average["b"].add_sample(b[i, :])
+
+        self.assertTrue(np.isclose(average["a"].mean(), np.mean(a)))
+        self.assertTrue(np.isclose(average["b"].mean(), np.mean(b)))
+
+    def test_StaticNd(self):
+
+        average = defaultdict(enstat.mean.StaticNd)
+
+        a = np.random.random(35 * 50 * 20).reshape(35, 50, 20)
+        b = np.random.random(37 * 52 * 21).reshape(37, 52, 21)
+
+        for i in range(a.shape[0]):
+            average["a"].add_sample(a[i, :, :])
+
+        for i in range(b.shape[0]):
+            average["b"].add_sample(b[i, :, :])
+
+        self.assertTrue(np.allclose(average["a"].mean(), np.mean(a, axis=0)))
+        self.assertTrue(np.allclose(average["b"].mean(), np.mean(b, axis=0)))
+        self.assertTrue(average["a"].shape() == a.shape[1:])
+        self.assertTrue(average["b"].shape() == b.shape[1:])
 
 
 if __name__ == "__main__":
