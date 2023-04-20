e446cfb9c485cf936ca993f170377f7d07ca1c3e
Tom de Geus <tom@geus.me>
2023-04-20 14:34:39 +0200

    minor style update

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,9 @@
 project = "enstat"
 copyright = "2021, Tom de Geus"
 author = "Tom de Geus"
+html_theme = "furo"
 autodoc_type_aliases = {"Iterable": "Iterable", "ArrayLike": "ArrayLike"}
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-html_theme = "furo"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
