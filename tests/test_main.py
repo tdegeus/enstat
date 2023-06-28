afbfd743c465fb1c246e266613cb0f5527c98aa3
Tom de Geus <tom@geus.me>
2023-06-28 15:30:04 +0200

    Adding `static.squash`

--- tests/test_main.py
+++ tests/test_main.py
@@ -234,6 +234,114 @@ class Test_mean(unittest.TestCase):
 
         self.assertTrue(np.all(np.equal(average.norm, np.sum(np.logical_not(m), axis=0))))
 
+    def test_static_squash_example(self):
+        norm = np.array([[2, 2, 3, 1, 1], [2, 2, 1, 1, 3], [1, 1, 2, 2, 1], [2, 1, 2, 2, 2]])
+        average = enstat.static(shape=norm.shape)
+        average.norm = norm
+        average.squash([2, 2])
+        expect = np.array([[8, 6, 4], [5, 8, 3]])
+        self.assertTrue(np.all(np.equal(average.norm, expect)))
+
+    def test_static_squash_1d_a(self):
+        a = np.random.random(13 * 2).reshape(13, 2)
+        average = enstat.static(shape=[a.size])
+        average += a.ravel()
+        average.squash(2)
+        self.assertTrue(np.allclose(average.mean(), np.mean(a, axis=1)))
+
+        e = np.random.random(1)
+        average = enstat.static(shape=[a.size + 1])
+        average += np.concatenate((a.ravel(), e))
+        average.squash(2)
+        self.assertTrue(np.allclose(average.mean(), np.concatenate((np.mean(a, axis=1), e))))
+
+    def test_static_squash_1d_b(self):
+        a = np.random.random(13 * 3).reshape(13, 3)
+        average = enstat.static(shape=[a.size])
+        average += a.ravel()
+        average.squash(3)
+        self.assertTrue(np.allclose(average.mean(), np.mean(a, axis=1)))
+
+        e = np.random.random(2)
+        average = enstat.static(shape=[a.size + 2])
+        average += np.concatenate((a.ravel(), e))
+        average.squash(3)
+        self.assertTrue(
+            np.allclose(
+                average.mean(), np.concatenate((np.mean(a, axis=1), np.array([np.mean(e)])))
+            )
+        )
+
+    def test_static_squash_2d_a(self):
+        a = np.random.random(12 * 8).reshape(12, 8)
+        b = 0.25 * (a[::2, ::2] + a[1::2, ::2] + a[::2, 1::2] + a[1::2, 1::2])
+        average = enstat.static(shape=a.shape)
+        average += a
+        average.squash([2, 2])
+        self.assertTrue(np.allclose(average.mean(), b))
+
+        a = np.random.random(12 * 8).reshape(12, 8)[:-1, :]
+        n = np.ones_like(a)
+        z = np.zeros((1, 4))
+        b = (
+            a[::2, ::2]
+            + np.vstack((a[1::2, ::2], z))
+            + a[::2, 1::2]
+            + np.vstack((a[1::2, 1::2], z))
+        )
+        n = (
+            n[::2, ::2]
+            + np.vstack((n[1::2, ::2], z))
+            + n[::2, 1::2]
+            + np.vstack((n[1::2, 1::2], z))
+        )
+        average = enstat.static(shape=a.shape)
+        average += a
+        average.squash([2, 2])
+        self.assertTrue(np.allclose(average.mean(), b / n))
+
+        a = np.random.random(12 * 8).reshape(12, 8)[:, :-1]
+        n = np.ones_like(a)
+        z = np.zeros((6, 1))
+        b = (
+            a[::2, ::2]
+            + a[1::2, ::2]
+            + np.hstack((a[::2, 1::2], z))
+            + np.hstack((a[1::2, 1::2], z))
+        )
+        n = (
+            n[::2, ::2]
+            + n[1::2, ::2]
+            + np.hstack((n[::2, 1::2], z))
+            + np.hstack((n[1::2, 1::2], z))
+        )
+        average = enstat.static(shape=a.shape)
+        average += a
+        average.squash([2, 2])
+        self.assertTrue(np.allclose(average.mean(), b / n))
+
+        a = np.random.random(12 * 8).reshape(12, 8)[:-1, :-1]
+        n = np.ones_like(a)
+        zr = np.zeros((1, 4))
+        zc = np.zeros((6, 1))
+        zc1 = np.zeros((5, 1))
+        b = (
+            a[::2, ::2]
+            + np.vstack((a[1::2, ::2], zr))
+            + np.hstack((a[::2, 1::2], zc))
+            + np.vstack((np.hstack((a[1::2, 1::2], zc1)), zr))
+        )
+        n = (
+            n[::2, ::2]
+            + np.vstack((n[1::2, ::2], zr))
+            + np.hstack((n[::2, 1::2], zc))
+            + np.vstack((np.hstack((n[1::2, 1::2], zc1)), zr))
+        )
+        average = enstat.static(shape=a.shape)
+        average += a
+        average.squash([2, 2])
+        self.assertTrue(np.allclose(average.mean(), b / n))
+
     def test_dynamic1d(self):
         """
         Dynamically grow shape.
